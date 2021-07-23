961549cca2c67da72b758071d3bc4d33c84cb294
Jizu Sun <jizu.sun@oqton.com>
2021-07-23 17:29:26 +0800

    add tests

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as apigw from "@aws-cdk/aws-apigateway";
+import { HitCounter } from './hitCounter';
+import { TableViewer } from "cdk-dynamo-table-viewer";
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -12,9 +14,18 @@ export class CdkWorkshopStack extends cdk.Stack {
       handler: 'hello.handler'
     })
 
-    new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
     })
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    })
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table,
+    });
 
   }
 }
---
+++ lib/hitCounter.ts
@@ -0,0 +1,44 @@
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
+
+
+export interface HitCounterProps {
+    downstream: lambda.IFunction;
+    readCapacity?:number;
+}
+
+export class HitCounter extends cdk.Construct {
+
+    public readonly handler: lambda.Function;
+    public readonly table: dynamodb.Table
+
+    constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
+
+        if (props.readCapacity !== undefined && (props.readCapacity < 5 || props.readCapacity > 20)) {
+            throw new Error('readCapacity must be greater than 5 and lower than 20');
+        }
+
+        super(scope, id)
+        const table = new dynamodb.Table(this, 'Hits', {
+            partitionKey: {name: 'path', type: dynamodb.AttributeType.STRING},
+            serverSideEncryption: true,
+            readCapacity: props.readCapacity ?? 5
+        })
+        this.table = table
+        this.handler = new lambda.Function(this, 'HitCounterHandler', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            handler: 'hitcounter.handler',
+            code: lambda.Code.fromAsset('lambda'),
+            environment: {
+                DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+                HITS_TABLE_NAME: table.tableName
+            }
+        })
+
+        table.grantReadWriteData(this.handler)
+        props.downstream.grantInvoke(this.handler)
+    }
+
+
+}
